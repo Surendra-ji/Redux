460ff4f978419e4800bf6e346ffaaaf16959afe0
Surendra Kumar <surendra.kumar@merkle.com>
2024-02-27 23:35:00 +0530

    redux learning

--- index.js
+++ index.js
@@ -7,8 +7,10 @@ const reducer = (store = INITIAL_VALUE, action) => {
         let newStore = store;
         if (action.type === "INCREMENT") {
             newStore = {counter: store.counter + 1};
-        } else if(action.type === "DICREMENT") {
+        } else if(action.type === "DECREMENT") {
             newStore = {counter: store.counter - 1};
+        } else if(action.type === "ADDITION") {
+            newStore = {counter : store.counter + action.payload.number};
         }
 
     return newStore;
@@ -24,6 +26,7 @@ store.subscribe(subscriber);
 
 store.dispatch({type: "INCREMENT"});
 store.dispatch({type: "INCREMENT"});
-store.dispatch({type: "DICREMENT"});
+store.dispatch({type: "DECREMENT"});
 store.dispatch({type: "INCREMENT"});
 store.dispatch({type: "INCREMENT"});
+store.dispatch({type: "ADDITION", payload : { number: 7}});
